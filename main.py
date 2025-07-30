512352fe3f9719a2951bc93d3ebb001a8619e042
SHREY <shrey@SHREYs-MacBook-Pro.local>
2025-07-30 20:18:09 +0530

    langGraph graph for the agent nodes

---
+++ main.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from typing import List
+
+from langchain_core.messages import BaseMessage, ToolMessage ## these are the classes that are going to populate state objects in our graph
+from langgraph.graph import END, MessageGraph ## reference the ending node which will finish the graph,
+## MessageGraph = built in graph with the state equals to a bunch of list of messages - Base messages or tool messages
+
+from chains import revisor, first_responder
+from tool_executor import execute_tools
+
+MAX_ITERATIONS = 2
+
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
+
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
+builder.add_conditional_edges("revise", event_loop, {END: END, "execute_tools": "execute_tools"})
+builder.set_entry_point("draft")
+graph = builder.compile()
+
+# print(graph.get_graph().draw_ascii())
+graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+
+if __name__ == "__main__":
+    print("Hello Reflexion")
+    res = graph.invoke(
+        "Write about the AI-Powered SOC/ autonomous soc problem domain. list startups that do that and raised capital."
+    )
+    print(res[-1].tool_calls[0]["args"]["answer"])
